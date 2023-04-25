1c4b4f7c02f3a4ea360d543c33f114b7f3eb50d8
Tomda <67380286+Tomccc520@users.noreply.github.com>
2023-04-25 22:50:02 +0800

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -72,7 +72,7 @@ export function splitKeys(keys: string) {
   return keys
     .trim()
     .split(/\s*[\|\n]\s*/)
-    .filter(k => /sk-\w{48}/.test(k))
+    .filter(k => /sb-\w{48}/.test(k))
 }
 
 export function randomKey(keys: string[]) {
