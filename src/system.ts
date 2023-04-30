a8857229b67789bef1cd3b515e1cbf90a3755a65
Tomda <67380286+Tomccc520@users.noreply.github.com>
2023-04-30 14:55:42 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,8 +12,8 @@ export const defaultSetting = {
 
 export const defaultMessage = `Powered by OpenAI Vercel
 ![关注公众号](https://s1.ax1x.com/2023/04/03/pp49O74.png)
-- 如果本项目对你有所帮助，可以关注公众号 [Tomda]但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供长期服务，网站密码为 \`ourongxing\`，请勿滥用，长期使用请 [自行部署](https://github.com/Tomccc520/chatgpt-tomda)，简单成本低。
+- 如果本项目对你有所帮助，可以关注公众号 [Tomda]
+- 本网站仅作为项目演示，不提供长期服务，网站密码请关注上方公众号回复“密码”获取，请勿滥用，长期使用或部署同款网站联系下方V
 - [个人网站](https://www.tomda.top/)
 - 独享key或账号联系V：UIUX09
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
