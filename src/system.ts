48446e21648badfc4119dd800668f2490b42c3f5
Tomda <67380286+Tomccc520@users.noreply.github.com>
2023-04-30 15:38:07 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,12 +10,13 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by OpenAI Vercel
-![关注公众号](https://s1.ax1x.com/2023/04/03/pp49O74.png)
+export const defaultMessage = `一个爱代码的设计师Tomda
+![关注公众号](https://s1.ax1x.com/2023/04/30/p93cj56.png)
+-🙇 亲爱的用户，因请求繁忙， 成本已失控。如您感觉好用可以赞赏支持，感谢您助力每一个梦想
 - 如果本项目对你有所帮助，可以关注公众号 [Tomda]
-- 本网站仅作为项目演示，不提供长期服务，网站密码请关注上方公众号回复“密码”获取，请勿滥用，长期使用或部署同款网站联系下方V
+- 网站密码请关注上方公众号回复“密码”获取，请勿滥用，长期使用或部署同款网站联系下方V
 - [个人网站](https://www.tomda.top/)
-- 独享key或账号联系V：UIUX09
+- AI交流群联系V：UIUX09
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
